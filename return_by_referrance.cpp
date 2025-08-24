426bfe3f72b75a435e9e6bc2a28bab92d4bf290f
Shivam Kumar <shivamguptaujjain211@gmail.com>
2025-08-24 17:59:30 +0530

    Update return_by_referrance.cpp

--- return_by_referrance.cpp
+++ return_by_referrance.cpp
@@ -9,11 +9,17 @@ int main()
 {
     int arr[5]={10,20,30,40,50};
 
-    cout<<"Before"<<arr[2]<<endl;
+    cout<<"Before:- "<<arr[4]<<endl;
 
-    getelement(arr,2)=100;
+    getelement(arr,4)=100;
 
-    cout<<"After"<<arr[2]<<endl;
+    cout<<"After:- "<<arr[4]<<endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"Array at index "<<i<<" will be:-"<<arr[i]<<endl;
+    }
+    
 
     return 0;
 }
